4c7a0450e7ce32631bc01f69dfd2b57380dc0564
Sabedi <nsab@edinger.org>
2016-10-18 13:08:05 +0200

    made a pug demo

---
+++ pug-demo/app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// Including the necessary modules
+const express = require( 'express')
+const fs = require('fs')
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+//views is views-directory!
+
+app.get('/ping', (request, response) => {
+	response.send('Pong')
+})
+
+app.get('/index', (request, response) => {
+	console.log('about to render a pug page!')
+	fs.readFile(__dirname + '/data.json', (error, data) => {
+		if (error) throw error
+		// error? throw error
+		let parsedData = JSON.parse(data)
+		console.log(parsedData)
+		response.render('index', {data: parsedData})
+	})
+	
+})
+
+app.listen(8000, () => {
+	console.log('Server running')
+})
